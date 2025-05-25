f89ce2962815710e0d45f3ab28a731b21f1b5fc6
Keith Kurak <keith.kurak@gmail.com>
2025-05-25 09:28:27 +0200

    fix multiple versions of rn

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,10 +35,7 @@ export default function RootLayout() {
               name="persona/chat"
               options={{ animation: 'slide_from_right' }}
             />
-            <Stack.Screen
-              name="tasks"
-              options={{ presentation: 'modal' }}
-            />
+            <Stack.Screen name="tasks" options={{ presentation: 'modal' }} />
             <Stack.Screen name="settings" options={{ presentation: 'modal' }} />
           </Stack>
         </ThemeProvider>
